9630d2a359caf1462381e970f3478f4d6ced673e
ArtemPonkratov <artemka.g@mail.ru>
2023-04-12 19:39:05 +0200

    part3

---
+++ main.js
@@ -0,0 +1,32 @@
+// // Задание 1
+
+const submitBtn = document.getElementById("submit-btn");
+submitBtn.
+submit
+
+addEventListener("click", function() {
+
+const usernameInput = document.querySelector('input[name="user"]');
+const passwordInput = document.querySelector('input[name="parol"]');
+  
+if (usernameInput.value === "artem" && passwordInput.value === "123") {
+console.log("Вход успешен");
+  } 
+else {
+
+console.log("Неверный логин или пароль");
+  }
+});
+
+// Задание 2.
+
+let num = 100;
+
+if (num > 100) {
+  document.write(num);
+} else {
+  console.log(num);
+}
+
+
+// Получаем ссылку на кнопку
